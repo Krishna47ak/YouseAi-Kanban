43109a14038438b8c79ca56aa5218032418532b9
Krishna47ak <anandakrishna720@gmail.com>
2024-09-20 04:39:31 +0530

    [ created: user mongoose model ]

---
+++ src/models/user.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: [true, "Please provide a phone number"]
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"]
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
