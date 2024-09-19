0bd246ecf402643eb72f60ebf3e6d6f30a6285d4
Krishna47ak <anandakrishna720@gmail.com>
2024-09-20 03:12:24 +0530

    [ created: landing screen ]

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-const Home = () => {
+const Landing = () => {
     return (
-        <div className='' >
-            Home
+        <div className="bg-[url('/kanban-bg.jpg')] bg-center bg-cover bg-fixed min-h-screen relative w-full overflow-hidden text-white">
+            <div className="flex h-full items-center p-20 min-h-screen">
+                <div className="">
+                    <div className="flex flex-col justify-center max-md:items-center space-y-4 max-md:text-center">
+                        <div className="space-y-2">
+                            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl">
+                                Unlock Your Productivity Potential
+                            </h2>
+                            <p className="max-w-[500px] md:text-xl text-gray-300">
+                                Transform your projects with our intuitive Kanban board and achieve success every day.
+                            </p>
+                        </div>
+                        <Link href="/signup">
+                            <Button>Start planning now &#8594;</Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Landing;
