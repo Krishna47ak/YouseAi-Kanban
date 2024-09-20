bf7aa406da8d4cfb3dc14e57ccb68288858037d2
Krishna47ak <anandakrishna720@gmail.com>
2024-09-20 05:34:43 +0530

    [ created: signup api route ]

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -2,9 +2,9 @@ import Navbar from "@/components/Navbar"
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <>
+        <div className="bg-gradient-to-bl from-[#1e0f50] via-[#9e1dacb2] to-[#1e0f50]" >
             <Navbar />
             {children}
-        </>
+        </div>
     )
 }
---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "@/models/user";
+import connectDB from "@/lib/DBConfig";
+
+connectDB();
+
+export async function POST(request: Request) {
+    try {
+        const reqBody = await request.json()
+        const { name, email, phone, password } = reqBody
+
+        const user = await User.findOne({ email })
+
+        if (user) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            name,
+            email,
+            phone,
+            password: hashedPassword
+        })
+
+        const savedUser = await newUser.save()
+
+        const tokenData = {
+            id: savedUser?._id,
+            email: savedUser?.email,
+        }
+
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "3d" })
+
+        const response = NextResponse.json({
+            message: "User created successfully",
+            user: { name, email, phone },
+            success: true,
+        })
+
+        response.cookies.set("token", token, {
+            httpOnly: true,
+        })
+
+        return response
+
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+
+    }
+}
