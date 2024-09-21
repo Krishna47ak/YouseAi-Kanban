87ebb7fd45a7c916867a5d9c3e961b236499fe70
Krishna47ak <anandakrishna720@gmail.com>
2024-09-21 08:34:13 +0530

    [ improvements ]

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,18 @@
 import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server';
 
-export function middleware(request) {
+
+export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
 
     const isPublicPath = path === '/login' || path === '/signup' || path === '/'
 
-    const isAdminPath = path === '/onboarding' || path === '/myboard'
+    const isAdminPath = path === '/onboarding' || path === '/myboard' || path === '/dashboard'
 
     const token = request.cookies.get('token')?.value || ''
+
+    console.log(token);
+
 
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/onboarding', request.nextUrl))
@@ -24,6 +29,7 @@ export const config = {
         '/login',
         '/signup',
         '/onboarding',
-        '/myboard'
+        '/myboard',
+        '/dashboard',
     ]
 }
